c5a79dea3983600b52229d9b09012f79dbc005b8
Mar3y <ahmed.mariee.2002@gmail.com>
2025-04-24 20:31:27 +0200

    routes out

--- routes/api.php
+++ routes/api.php
@@ -16,20 +16,20 @@ Route::post('/login',               [AuthController::class, 'login']);
 Route::get('/auth/google',          [AuthController::class, 'redirectToGoogle']);
 Route::get('/auth/google/callback', [AuthController::class, 'handleGoogleCallback']);
 
+Route::get('/cars',      [CarController::class, 'index']);
+Route::get('/cars/{id}', [CarController::class, 'show']);
+Route::get('/plates',      [PlateController::class, 'index']);
+Route::get('/plates/{id}', [PlateController::class, 'show']);
+Route::get('/hero', [HeroController::class, 'getHero']);
 Route::middleware(['auth.client'])->group(function () {
     Route::post('/logout',              [AuthController::class, 'logout']);
 
     
-    Route::get('/cars',      [CarController::class, 'index']);
-    Route::get('/cars/{id}', [CarController::class, 'show']);
     Route::post('/cars',     [CarController::class, 'store']);
 
     // Plate Routes
-    Route::get('/plates',      [PlateController::class, 'index']);
-    Route::get('/plates/{id}', [PlateController::class, 'show']);
     Route::post('/plates',     [PlateController::class, 'store']);
 
     // Hero Routes
-    Route::get('/hero', [HeroController::class, 'getHero']);
 });
 
